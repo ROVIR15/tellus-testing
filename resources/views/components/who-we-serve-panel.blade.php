666a33cfd56a0cb4d855ea1db5513bb23b34ec1c
ROVIR <ekoarieanto92@gmail.com>
2025-12-28 08:39:07 +0700

    style(who-we-serve-panel): adjust responsive st

--- resources/views/components/who-we-serve-panel.blade.php
+++ resources/views/components/who-we-serve-panel.blade.php
@@ -1,7 +1,7 @@
 @props(['title', 'description', 'icon' => '🌀', 'gap' => '32px', 'angle' => '0deg', 'opacity' => '1', 'borderRadius' => '24px', 'padding' => '40px', 'rowStart' => 1, 'colStart' => 1, 'colSpan' => 1, 'rowSpan' => 2])
 
 <!-- Who We Serve Left Panel -->
-<div {{ $attributes->merge(['class' => 'shadow-md hover:shadow-lg transition-shadow duration-300 flex flex-col gap-8 justify-start p-8 rounded-4xl']) }}
+<div {{ $attributes->merge(['class' => 'shadow-md hover:shadow-lg transition-shadow duration-300 flex flex-col gap-8 justify-start p-8 rounded-4xl md:h-full']) }}
     style="background: radial-gradient(3072.21% 55% at 100% 0%, #76B2E8 0%, #006ACC 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;">
     <!-- Icon Circle -->
     <div class="flex justify-start">
@@ -12,7 +12,7 @@
 
     <div class="flex flex-col gap-8">
         <!-- Heading -->
-        <x-heading-display-3 class="text-start md:text-left" style="color: var(--color-primary-100);">
+        <x-heading-display-3 class="custom-height text-start md:text-left md:size-fit" style="color: var(--color-primary-100);">
             Who We Serve
         </x-heading-display-3>
 
